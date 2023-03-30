73c34d5baed86178cbd9552e990270d851d2e3bc
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-03-30 05:33:11 +0100

    changed type declarations

--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,8 +1,8 @@
-interface DieProps {
+type DieProps = {
     number: number;
 }
 
-export default function Die(props: DieProps) {
+export default function Die(props: DieProps): JSX.Element {
     return (
         <div>{props.number}</div>
     )
