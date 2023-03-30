b77e2738a77fd2c58a2acf019abd633efc245936
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-03-30 20:10:56 +0100

    re-roll button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
+import { useState } from "react"
 import Die from "./components/Die"
 
 function App(): JSX.Element {
 
+    function genDice(): number[] {
+        return Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6))
+    }
+
+    const [dice, setDice] = useState(genDice)
+    const diceElems = dice.map(num => <Die number={num} />)
+
+    function roll() {
+        setDice(genDice())
+    }
+
     return (
         <main>
             <div className="die--tray">
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
-                <Die number={Math.floor(Math.random() * 6) + 1} />
+                {diceElems}
             </div>
+            <button className="roll" onClick={roll}>Roll</button>
         </main>
     )
 }
