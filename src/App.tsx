dc3301764d15417009b40c8d3252149e9269494f
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-04-04 04:20:20 +0100

    add instructions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Die from "./components/Die"
 
 function App() {
@@ -16,6 +16,21 @@ function App() {
     const [dice, setDice] = useState(genDice)
     const diceElems = dice.map(die => <Die key={die.id} {...die} />)
 
+    const [tsenzies, setTsenzies] = useState(false)
+
+    useEffect(() => {
+        //console.log("diceState changed")
+
+        if (dice.every(({ isHeld, value }) => isHeld && value == dice[0].value)) {
+            setTsenzies(true)
+            console.log("you win")
+        }
+
+        return () => {
+            //
+        }
+    }, [dice])
+
     function roll(): void {
         setDice(dice => dice.map(die => (die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) })))
     }
@@ -26,6 +41,8 @@ function App() {
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="die--tray">
                 {diceElems}
             </div>
